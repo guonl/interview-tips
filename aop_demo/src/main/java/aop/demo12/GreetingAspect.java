822119fb8ada5d6919ab6f1fa11dff7e60a792c8
guonl <983220871@qq.com>
2018-04-28 17:58:54 +0800

    添加一点注释

--- aop_demo/src/main/java/aop/demo12/GreetingAspect.java
+++ aop_demo/src/main/java/aop/demo12/GreetingAspect.java
@@ -9,6 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class GreetingAspect {
 
+    /**
+     * AspectJ execution 表达式拦截方法
+     * execution(* aop.demo.GreetingImpl.*(..))
+     *      execution()：表示拦截方法，括号中可定义需要匹配的规则。
+     *      第一个“*”：表示方法的返回值是任意的。
+     *      第二个“*”：表示匹配该类中所有的方法。
+     *      (..)：表示方法的参数是任意的。
+     *
+     * @param pjp
+     * @return
+     * @throws Throwable
+     */
     @Around("execution(* aop.demo12.GreetingImpl.*(..))")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
         before();
